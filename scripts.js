137766daa6c9c4ad1ec27ae6fe58f60b73a3256b
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-18 09:47:19 +0300

    add functionality to switch the theme

---
+++ scripts.js
@@ -0,0 +1,29 @@
+console.log("JS Works!!")
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for(let i = 0; i < themeDots.length; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode
+        console.log("Clicked", mode);
+        setTheme(mode)
+    })
+};
+
+function setTheme(mode) {
+    if(mode == 'light') {
+        document.getElementById('theme-style').href = 'default.css'
+    }
+
+    if(mode == 'slate') {
+        document.getElementById('theme-style').href = 'slate.css'
+    }
+
+    if(mode == 'green') {
+        document.getElementById('theme-style').href = 'green.css'
+    }
+
+    if(mode == 'purple') {
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+};
